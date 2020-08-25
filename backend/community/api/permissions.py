09262f53bd9d9cbcb60e0a392af0438927ea6ba8
Michail Gurbanov <54814200+reqww@users.noreply.github.com>
2020-08-25 15:24:20 +0300

    friends actions andpermission

--- backend/community/api/permissions.py
+++ backend/community/api/permissions.py
@@ -8,8 +8,8 @@ class IsCurrentUser(BasePermission):
     def has_permission(self, request, view):
         contact = get_object_or_404(Contact, pk=view.kwargs["pk"])
         return any([
-            (request.user and (request.user.username == contact.user.username)),
-            (view.request.user.is_superuser)
+            (request.user and (request.user.username==contact.user.username)),
+            (request.user and request.user.is_superuser)
         ])
 
 class IsUsersInvites(BasePermission):
@@ -18,5 +18,50 @@ class IsUsersInvites(BasePermission):
         username = request.query_params.get('username', None)
         return any([
             (request.user and request.user.username==username),
-            (request.user.is_superuser)
+            (request.user and request.user.is_superuser)
         ])
+
+class OneOfUsers(BasePermission):
+    '''Либо получатель, либо отправитель'''
+    def has_permission(self, request, view):
+        data = request.data
+        try:
+            sender_id = data['sender']
+            receiver_id = data['receiver']
+        except KeyError:
+            return True
+        sender_user = get_object_or_404(Contact, id=sender_id).user
+        receiver_user = get_object_or_404(Contact, id=receiver_id).user
+        return any([
+            (request.user==sender_user),
+            (request.user==receiver_user)
+        ])
+
+class IsReceiver(BasePermission):
+    '''Получатель ли?'''
+    def has_permission(self, request, view):
+        data = request.data
+        try:
+            receiver_id = data['receiver']
+        except KeyError:
+            return True
+        receiver_user = get_object_or_404(Contact, id=receiver_id).user
+        return bool(request.user and (request.user==receiver_user))
+
+class IsFriends(BasePermission):
+    '''Друзья ли?'''
+    def has_permission(self, request, view):
+        data = request.data
+        try:
+            sender_id = data['sender']
+            receiver_id = data['receiver']
+        except KeyError:
+            return True
+        sender_contact = get_object_or_404(Contact, id=sender_id)
+        receiver_contact = get_object_or_404(Contact, id=receiver_id)
+        return all([
+            (request.user),
+            (receiver_contact in sender_contact.friends.all()),
+            (sender_contact in receiver_contact.friends.all())
+        ])
+        
--- backend/community/api/urls.py
+++ backend/community/api/urls.py
@@ -1,11 +1,29 @@
 from rest_framework.routers import DefaultRouter
+from rest_framework.urlpatterns import format_suffix_patterns
 from django.urls import path
 
-from .views import ContactCustomViewSet, AddRequestCustomViewset
+from .views import (
+    ContactCustomViewSet, 
+    AddRequestCustomViewset, 
+    FriendPermissionViewset
+)
+
+friends_add = FriendPermissionViewset.as_view({
+    'post': 'add',
+})
+
+friends_remove = FriendPermissionViewset.as_view({
+    'post': 'remove',
+})
 
 urlpatterns = [
-    # path('friends/send/', AddRequestCreateView.as_view())
-]
+    
+] 
+
+urlpatterns += format_suffix_patterns([
+    path('friends/add/', friends_add, name='friends_remove'),
+    path('friends/remove/', friends_remove, name='friends_add')
+])
 
 r = DefaultRouter()
 r.register(r'request', AddRequestCustomViewset, basename='request')
