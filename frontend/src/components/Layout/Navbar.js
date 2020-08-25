014585d8d1e7ee52e93030f5e478cf15d8d6e0d6
unknown <0r20@mail.ru>
2020-08-25 15:11:57 +0300

    Исправление роутинга и навбара при перезагрузке страницы

--- frontend/src/components/Layout/Navbar.js
+++ frontend/src/components/Layout/Navbar.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import { Menu } from 'antd';
 import { MailOutlined, SettingOutlined, CommentOutlined, HomeOutlined, UserOutlined, TeamOutlined } from '@ant-design/icons';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import styled, { css } from 'styled-components';
 import useWidth from '../../hoc/useWidth';
+import useReactRouter from 'use-react-router';
 
 const navitems = [
   { key: 'my_page', name: 'Моя страница' },
@@ -24,16 +25,19 @@ const renderIcons = (key) => {
 }
 
 const renderNavItems = (navitems) => {
+
+  const { location, match } = useReactRouter();
+
   return (
     navitems.map(navitem => (
-      <Menu.Item
+      <NavLink
         key={navitem.key}
-        icon={renderIcons(navitem.key)}
+        to={`/${navitem.key}`}
+        className="nav-item"
       >
-        <Link to={`/${navitem.key}`}>
-          {navitem.name}
-        </Link>
-      </Menu.Item>
+        <div>{renderIcons(navitem.key)}</div>
+        <div>{navitem.name}</div>
+      </NavLink>
     ))
   )
 }
@@ -42,14 +46,7 @@ const Navbar = () => {
 
   return (
     <StyledNavbar>
-      <Menu
-        style={{ width: '100%' }}
-        defaultSelectedKeys={['dialogs']}
-        mode="inline"
-        inlineCollapsed={useWidth() < 575.98 ? true : false}
-      >
-        {renderNavItems(navitems)}
-      </Menu>
+      {renderNavItems(navitems)}
     </StyledNavbar>
   );
 }
@@ -61,10 +58,37 @@ const StyledNavbar = styled.div`
   position: fixed;
   height: 100%;
   border-right: 1px solid #f0f0f0;
+  display: flex;
+  flex-direction: column;
   .ant-menu {
     border: none;
   }
   @media (max-width: 575.98px) {
     width: auto;
+  }
+  .nav-item {
+    display: flex;
+    padding: 20px;
+    color: black;
+    opacity: 0.6;
+    @media (max-width: 575.98px) {
+      padding-left: 5px;
+    }
+    &.active {
+      color: #1890ff;
+    }
+    > div {
+      &:first-of-type {
+        margin-right: 20px;
+        @media (max-width: 575.98px) {
+          margin: 0;
+        }
+      }
+      &:last-of-type {
+        @media (max-width: 575.98px) {
+          display: none;
+        }
+      }
+    }
   }
 `;
