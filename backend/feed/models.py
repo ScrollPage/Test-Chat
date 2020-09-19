c871d447a7aaec1662a3e66800757a74edeede91
Michail Gurbanov <54814200+reqww@users.noreply.github.com>
2020-09-19 14:20:10 +0300

    post models fix

--- backend/feed/models.py
+++ backend/feed/models.py
@@ -2,6 +2,27 @@ from django.db import models
 
 from contact.models import Contact
 
+class Post(models.Model):
+    '''Обычный пост на стенку'''
+    parent = models.ForeignKey(
+        'self', 
+        verbose_name = 'Родитель', 
+        on_delete = models.SET_NULL, 
+        null = True, 
+        blank = True,
+        related_name = 'children'
+    )
+    user = models.ForeignKey(Contact, on_delete=models.DO_NOTHING)
+    text = models.TextField(max_length=1000, blank=True, default='')
+    image = models.ImageField(upload_to='user_posts/%Y/%m/%d', blank=True, null=True)
+
+    def __str__(self):
+        return str(self.user)
+
+    class Meta:
+        verbose_name = 'Пост'
+        verbose_name_plural = 'Посты'
+
 class Like(models.Model):
     '''Стандартный лайк'''
     user = models.ForeignKey(Contact, on_delete=models.CASCADE)
@@ -26,24 +47,4 @@ class RePost(models.Model):
         verbose_name = 'Репост'
         verbose_name_plural = 'Репосты'
 
-class Post(models.Model):
-    '''Обычный пост на стенку'''
-    parent = models.ForeignKey(
-        'self', 
-        verbose_name = 'Родитель', 
-        on_delete = models.SET_NULL, 
-        null = True, 
-        blank = True,
-        related_name = 'children'
-    )
-    user = models.ForeignKey(Contact, on_delete=models.DO_NOTHING)
-    text = models.TextField(max_length=1000, blank=True, default='')
-    image = models.ImageField(upload_to='user_posts/%Y/%m/%d', blank=True, null=True)
-
-    def __str__(self):
-        return str(self.user)
-
-    class Meta:
-        verbose_name = 'Пост'
-        verbose_name_plural = 'Посты'
 
