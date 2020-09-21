fc31ff6e973011e40fc0ac832d24bddb8a3b2d14
Michail Gurbanov <54814200+reqww@users.noreply.github.com>
2020-09-22 00:45:11 +0300

    parties (app)

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     'contact',
     'feed',
     'notifications',
+    'parties',
     'score',
 ]
 
--- backend/backend/yasg.py
+++ backend/backend/yasg.py
@@ -5,7 +5,7 @@ from drf_yasg import openapi
 
 schema_view = get_schema_view(
     openapi.Info(
-        title = 'Just Chat',
+        title = 'ScrollChat',
         default_version = 'v1',
         description = 'Chat description',
         license = openapi.License(name = 'BSD License'),
