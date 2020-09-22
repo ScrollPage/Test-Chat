b658b7c6c42ce13cd6b07a5b6556c92d338def77
Michail Gurbanov <54814200+reqww@users.noreply.github.com>
2020-09-22 20:06:36 +0300

    Update service.py

--- backend/notifications/service.py
+++ backend/notifications/service.py
@@ -4,28 +4,28 @@ def send_message_notifications(chat, sender):
     for user in chat.participant.all():
         if user != sender:
             pusher.trigger(
-                f'notifications_{user.id}', 
+                f'notifications', 
                 'new_message', 
                 {'sender': sender.id, 'chat_id': chat.id, 'name': sender.get_full_name()}
             )
 
 def send_addrequest_notification(sender, receiver):
     pusher.trigger(
-        f'notifications_{receiver.id}', 
+        f'notifications', 
         'new_request', 
         {'sender': sender.id, 'name': sender.get_full_name()}
     )
 
 def new_friend_notification(sender, receiver):
     pusher.trigger(
-        f'notifications_{sender.id}', 
+        f'notifications', 
         'new_friend', 
         {'sender': receiver.id, 'name': receiver.get_full_name()}
     )
 
 def send_like_notification(owner, liker, post):
     pusher.trigger(
-        f'notifications_{owner.id}', 
+        f'notifications', 
         'new_like', 
         {'liker': liker.id, 'post': post.id, 'name': liker.get_full_name()}
     )
