e7f35e5dab4cad4db113bc66340451877eae66cb
Michail Gurbanov <54814200+reqww@users.noreply.github.com>
2020-09-28 21:57:17 +0300

    fix

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -214,8 +214,8 @@ DJOSER = {
 }
 
 # Media
-# MEDIA_URL = '/media/'
-# MEDIA_ROOT = os.path.join(BASE_DIR, 'media') 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media') 
 
 #User model
 AUTH_USER_MODEL = 'contact.Contact'
@@ -231,10 +231,10 @@ pusher_client = pusher.Pusher(
 
 # AWS
 
-AWS_ACCESS_KEY_ID = "AKIAX3EUZUQFIQH62JQO"
-AWS_SECRET_ACCESS_KEY = "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"
-AWS_STORAGE_BUCKET_NAME = 'scroll-chat-media'
+# AWS_ACCESS_KEY_ID = "AKIAX3EUZUQFIQH62JQO"
+# AWS_SECRET_ACCESS_KEY = "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"
+# AWS_STORAGE_BUCKET_NAME = 'scroll-chat-media'
 
-AWS_S3_FILE_OVERWRITE = False
-AWS_DEFAULT_ACL = None
-DEFAULT_FILE_STORAGE = 'backend.storage_backends.MediaStorage'
+# AWS_S3_FILE_OVERWRITE = False
+# AWS_DEFAULT_ACL = None
+# DEFAULT_FILE_STORAGE = 'backend.storage_backends.MediaStorage'
